b4c9e02e68cb0591bfe9c8e80622dcbc2dbfa424
barnatemesi <98287245+barnatemesi@users.noreply.github.com>
2022-02-12 18:54:33 +0100

    Add files via upload

--- controllers.m
+++ controllers.m
@@ -1,18 +1,17 @@
 %Controllers
-%PI parameters
 
-switch 4
+switch 6
     case 1
         
         Ki_speed          = 2;
         Kp_speed          = 0.06;
         Ka_speed          = 0;
         
-        Ki_id          = 463;
-        Kp_id          = 3.8;
+        Ki_id          = 400;
+        Kp_id          = 4;
         
-        Ki_iq          = 463;
-        Kp_iq          = 3.8;
+        Ki_iq          = 400;
+        Kp_iq          = 4;
         
     case 2
         %
@@ -51,9 +50,21 @@ switch 4
         Kp_iq          = 3.8;
         
     case 5
-        %Lab values
         Ki_speed          = 1;
         Kp_speed          = 0.1;
+        Ka_speed          = 5;
+        
+        Ki_id          = 463;
+        Kp_id          = 3.8;
+        
+        Ki_iq          = 463;
+        Kp_iq          = 3.8;
+        
+    case 6
+        %PID
+        Ki_speed          = 3;
+        Kp_speed          = 0.4;
+        Kd_speed          = 0.1;
         Ka_speed          = 5;
         
         Ki_id          = 463;
